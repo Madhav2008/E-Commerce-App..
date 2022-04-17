acb779484a0d9aac8c6acbf468411782e839e23e
arora <aroramadhav65@gmail.com>
2022-04-17 14:26:52 +0530

    Update

--- lib/screens/home/components/categories.dart
+++ lib/screens/home/components/categories.dart
@@ -7,7 +7,7 @@ class Categories extends StatelessWidget {
   Widget build(BuildContext context) {
     List<Map<String, dynamic>> categories = [
       {"icon": "../assets/icons/Flash Icon.svg", "text": "Flash Deal"},
-      {"icon": "../assets/icons/Bill Icon.svg", "text": "Bill"},
+      {"icon": "../assets/icons/BillIcon.svg", "text": "Bill"},
       {"icon": "../assets/icons/GameIcon.svg", "text": "Game"},
       {"icon": "../assets/icons/Gift Icon.svg", "text": "Daily Gift"},
       {"icon": "../assets/icons/Discover.svg", "text": "More"},

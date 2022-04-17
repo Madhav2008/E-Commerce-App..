953895b9a26eb6d8a5ffc24559e470a474ac290a
arora <aroramadhav65@gmail.com>
2022-04-17 14:37:19 +0530

    Update

--- lib/screens/profile/components/profile_pic.dart
+++ lib/screens/profile/components/profile_pic.dart
@@ -34,7 +34,7 @@ class ProfilePic extends StatelessWidget {
                   backgroundColor: Color(0xFFF5F6F9),
                 ),
                 onPressed: () {},
-                child: SvgPicture.asset("../assets/icons/Camera Icon.svg"),
+                child: SvgPicture.asset("../assets/icons/CameraIcon.svg"),
               ),
             ),
           )

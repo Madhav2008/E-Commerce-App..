563e4c172fe814db3b56a1050365846fa636b816
arora <aroramadhav65@gmail.com>
2022-04-17 13:41:04 +0530

    Update

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:e_commerce_app_2/screens/splash/components/body.dart';
-import 'package:shop_app/size_config.dart';
+import 'package:e_commerce_app_2/size_config.dart';
 
 class SplashScreen extends StatelessWidget {
   static String routeName = "/splash";

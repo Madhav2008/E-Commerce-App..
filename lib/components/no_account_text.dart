b4a895680717d09b6eaabac2e0415fd6ec234f4c
arora <aroramadhav65@gmail.com>
2022-04-17 13:11:53 +0530

    Update

--- lib/components/no_account_text.dart
+++ lib/components/no_account_text.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shop_app/screens/sign_up/sign_up_screen.dart';
+import 'package:e_commerce_app_2/screens/sign_up/sign_up_screen.dart';
 
 import '../constants.dart';
 import '../size_config.dart';

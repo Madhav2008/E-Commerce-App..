a0a1ca5d1bb6ab5e709b91302c2a91ff9e1ab728
arora <aroramadhav65@gmail.com>
2022-04-17 13:42:39 +0530

    Update

--- lib/screens/sign_up/components/body.dart
+++ lib/screens/sign_up/components/body.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:e_commerce_app_2/components/socal_card.dart';
 import 'package:e_commerce_app_2/constants.dart';
 import 'package:e_commerce_app_2/size_config.dart';
-
 import 'sign_up_form.dart';
 
 class Body extends StatelessWidget {

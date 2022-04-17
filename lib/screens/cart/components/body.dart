fb9d63ff78d48a32e7954b9bf22eff3bdcae9347
arora <aroramadhav65@gmail.com>
2022-04-17 14:11:34 +0530

    Update

--- lib/screens/cart/components/body.dart
+++ lib/screens/cart/components/body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:shop_app/models/Cart.dart';
+import 'package:e_commerce_app_2/models/Cart.dart';
 
 import '../../../size_config.dart';
 import 'cart_card.dart';

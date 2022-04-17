21d296f6ae89308fae9c8fadb91a963ad669480f
arora <aroramadhav65@gmail.com>
2022-04-17 13:41:09 +0530

    Update

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -6,7 +6,6 @@ class SplashScreen extends StatelessWidget {
   static String routeName = "/splash";
   @override
   Widget build(BuildContext context) {
-    // You have to call it on your starting screen
     SizeConfig().init(context);
     return Scaffold(
       body: Body(),

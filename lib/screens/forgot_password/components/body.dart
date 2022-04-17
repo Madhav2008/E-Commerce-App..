06121ff54a3698f0406cb041487788aed2cbad20
arora <aroramadhav65@gmail.com>
2022-04-17 14:04:07 +0530

    Update

--- lib/screens/forgot_password/components/body.dart
+++ lib/screens/forgot_password/components/body.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:e_commerce_app_2/components/custom_surfix_icon.dart';
 import 'package:e_commerce_app_2/components/default_button.dart';
 import 'package:e_commerce_app_2/components/form_error.dart';
-import 'package:shop_app/components/no_account_text.dart';
+import 'package:e_commerce_app_2/components/no_account_text.dart';
 import 'package:shop_app/size_config.dart';
 
 import '../../../constants.dart';

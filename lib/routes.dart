b858441d10d7cd89079272c65ac8c1d65f614a7a
arora <aroramadhav65@gmail.com>
2022-04-17 14:13:48 +0530

    Update

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,7 +3,7 @@ import 'package:e_commerce_app_2/screens/cart/cart_screen.dart';
 import 'package:e_commerce_app_2/screens/complete_profile/complete_profile_screen.dart';
 import 'package:e_commerce_app_2/screens/details/details_screen.dart';
 import 'package:e_commerce_app_2/screens/forgot_password/forgot_password_screen.dart';
-import 'package:shop_app/screens/home/home_screen.dart';
+import 'package:e_commerce_app_2/screens/home/home_screen.dart';
 import 'package:shop_app/screens/login_success/login_success_screen.dart';
 import 'package:shop_app/screens/otp/otp_screen.dart';
 import 'package:shop_app/screens/profile/profile_screen.dart';

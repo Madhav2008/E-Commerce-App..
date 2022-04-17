2313b03e52b4d8e808d5473e681f162d0f441786
arora <aroramadhav65@gmail.com>
2022-04-17 13:49:37 +0530

    Update

--- lib/screens/otp/components/otp_form.dart
+++ lib/screens/otp/components/otp_form.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:e_commerce_app_2/components/default_button.dart';
-import 'package:shop_app/size_config.dart';
+import 'package:e_commerce_app_2/size_config.dart';
 
 import '../../../constants.dart';
 

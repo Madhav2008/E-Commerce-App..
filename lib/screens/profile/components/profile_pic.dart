d4f23ec2c3a84ccffbceb1731f7b8bcd26072047
arora <aroramadhav65@gmail.com>
2022-04-17 14:37:11 +0530

    Update

--- lib/screens/profile/components/profile_pic.dart
+++ lib/screens/profile/components/profile_pic.dart
@@ -16,7 +16,7 @@ class ProfilePic extends StatelessWidget {
         clipBehavior: Clip.none,
         children: [
           CircleAvatar(
-            backgroundImage: AssetImage("../assets/images/Profile Image.png"),
+            backgroundImage: AssetImage("../assets/images/ProfileImage.png"),
           ),
           Positioned(
             right: -16,

25c183cf8cd533753f30c3aa4c07b9c710adad21
arora <aroramadhav65@gmail.com>
2022-04-17 13:48:57 +0530

    Update

--- lib/screens/otp/components/body.dart
+++ lib/screens/otp/components/body.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:e_commerce_app_2/constants.dart';
 import 'package:e_commerce_app_2/size_config.dart';
-
 import 'otp_form.dart';
 
 class Body extends StatelessWidget {

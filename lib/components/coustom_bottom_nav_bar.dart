44139b128a95803793bb58af9d13e716b64d005f
arora <aroramadhav65@gmail.com>
2022-04-17 13:09:33 +0530

    Update

--- lib/components/coustom_bottom_nav_bar.dart
+++ lib/components/coustom_bottom_nav_bar.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:shop_app/screens/home/home_screen.dart';
+import 'package:e_commerce_app_2/screens/home/home_screen.dart';
 import 'package:shop_app/screens/profile/profile_screen.dart';
 
 import '../constants.dart';

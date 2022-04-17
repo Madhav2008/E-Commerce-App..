ff44b97ed1cda19ff04def1150a71e3242d1be5b
arora <aroramadhav65@gmail.com>
2022-04-17 14:13:26 +0530

    Update

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/widgets.dart';
-import 'package:shop_app/screens/cart/cart_screen.dart';
+import 'package:e_commerce_app_2/screens/cart/cart_screen.dart';
 import 'package:shop_app/screens/complete_profile/complete_profile_screen.dart';
 import 'package:shop_app/screens/details/details_screen.dart';
 import 'package:shop_app/screens/forgot_password/forgot_password_screen.dart';

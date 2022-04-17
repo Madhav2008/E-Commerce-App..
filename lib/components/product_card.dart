3ead3e7e3b3915c755024161d0232ff9ded52370
arora <aroramadhav65@gmail.com>
2022-04-17 13:12:38 +0530

    Update

--- lib/components/product_card.dart
+++ lib/components/product_card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:e_commerce_app_2/models/Product.dart';
-import 'package:shop_app/screens/details/details_screen.dart';
+import 'package:e_commerce_app_2/screens/details/details_screen.dart';
 
 import '../constants.dart';
 import '../size_config.dart';
